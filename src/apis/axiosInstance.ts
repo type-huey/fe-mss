d61f1c074498741788be791230bd95bd5c3e70cb
sanghun <sanghun33@naver.com>
2023-02-10 19:48:00 +0900

    feat(axios) axiosinstance 생성

---
+++ src/apis/axiosInstance.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+export const request = (config: AxiosRequestConfig) => {
+  return new Promise((resolve, reject) => {
+    axios({
+      baseURL: import.meta.env.VITE_API_BASE_URL,
+      withCredentials: false,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      ...config
+    })
+      .then(response => {
+        if (
+          response.status === 200 ||
+          response.status === 201 ||
+          response.status === 204
+        )
+          resolve(response.data.data);
+        else reject(response);
+      })
+      .catch(error => {
+        console.log(error.response);
+        reject(error.response);
+      });
+  });
+};
